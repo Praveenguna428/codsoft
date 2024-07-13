819a782a0ada62e6cdbdca1976c7f8c95237bc4e
Praveen Kumar <144327965+Praveenguna428@users.noreply.github.com>
2024-07-13 16:30:40 +0530

    Create password generator.py
    A simple yet powerful password generator tool built with Python. This script generates strong and random passwords based on user input for length and complexity.

---
+++ password generator.py
@@ -0,0 +1,45 @@
+
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) 
+    for i in range(length))
+    return password
+
+def main():
+    try:
+        print("   -------------------------------------------------------------   ")
+        print("   ================== PYTHON PASSWORD GENERATOR ================    ")
+        print("   -------------------------------------------------------------   ")
+    
+        length = int(input("   Enter the desired length of the password : "))
+        print("   -------------------------------------------------------------    ")
+        if length < 8 or length > 128:
+            print("Password length must be between 8 and 128 characters.")
+        else:
+            complexity = input("   Choose password complexity (weak/medium/strong): ")
+            print("   _____________________________________________________________   ")
+
+            complexity=complexity.lower()
+            if complexity == "weak":
+                characters = string.ascii_letters
+            elif complexity == "medium":
+                characters = string.ascii_letters + string.digits
+            elif complexity == "strong":
+                characters = string.ascii_letters + string.digits + string.punctuation
+            else:
+                print("Invalid complexity level. Using strong complexity by default.")
+                characters = string.ascii_letters + string.digits + string.punctuation
+            password = ''.join(random.choice(characters) for i in range(length))
+            print("                Generated Password:", password)
+            print("   _____________________________________________________________   ")
+            print("   ============= YOUR PASSWORD GENERATED SUCCESSFULLY ==========    ")
+
+            
+    except ValueError:
+        print("Please enter a valid number for the password length.")
+
+if __name__ == "__main__":
+    main()
